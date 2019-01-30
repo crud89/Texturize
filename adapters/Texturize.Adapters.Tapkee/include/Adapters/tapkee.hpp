7c834d533afd3a051389b9dffc6c2e58242cf1df
Carsten <18394207+Aschratt@users.noreply.github.com>
2019-01-30 16:16:39 +0100

    First PCA descriptor extractor implementation for tapkee adapter.

--- adapters/Texturize.Adapters.Tapkee/include/Adapters/tapkee.hpp
+++ adapters/Texturize.Adapters.Tapkee/include/Adapters/tapkee.hpp
@@ -1,5 +1,35 @@
 #pragma once
 
+#pragma warning( disable: 4267 4244 )
+
 #include <texturize.hpp>
+#include <sampling.hpp>
 
 #include <tapkee/tapkee.hpp>
+
+namespace Texturize {
+	namespace Tapkee {
+
+		/// \defgroup tapkee Tapkee Adapter
+		/// Contains different dimensionality reductors implemented in Tapkee.
+		/// @{
+
+		class TEXTURIZE_API PCADescriptorExtractor :
+			public DescriptorExtractor {
+			// IDescriptorExtractor
+		public:
+			cv::Mat calculateNeighborhoodDescriptors(const Sample& exemplar) const override;
+			cv::Mat calculateNeighborhoodDescriptors(const Sample& exemplar, const cv::Mat& uv) const override;
+		};
+
+		class TEXTURIZE_API SNEDescriptorExtractor :
+			public DescriptorExtractor {
+			// IDescriptorExtractor
+		public:
+			cv::Mat calculateNeighborhoodDescriptors(const Sample& exemplar) const override;
+			cv::Mat calculateNeighborhoodDescriptors(const Sample& exemplar, const cv::Mat& uv) const override;
+		};
+
+		/// @}
+	}
+}
---
+++ adapters/Texturize.Adapters.Tapkee/src/DescriptorExtractor.cpp
@@ -0,0 +1,74 @@
+#include "stdafx.h"
+
+#include <Adapters/tapkee.hpp>
+#include <sampling.hpp>
+
+#include <Eigen/Eigen>
+#include <Eigen/Dense>
+#include <opencv2/core/eigen.hpp>
+
+using namespace Texturize;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///// PCA descriptor extractor implementation                                                 /////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+cv::Mat Tapkee::PCADescriptorExtractor::calculateNeighborhoodDescriptors(const Sample& exemplar) const
+{
+	// Create a UV-Map for the sample.
+	cv::Mat uv = this->createContinuousUvMap(exemplar);
+
+	// Calculate the descriptors.
+	return this->calculateNeighborhoodDescriptors(exemplar, uv);
+}
+
+cv::Mat Tapkee::PCADescriptorExtractor::calculateNeighborhoodDescriptors(const Sample& exemplar, const cv::Mat& uv) const
+{
+	//tapkee::float_distance_callback distanceCallback;
+	//tapkee::float_kernel_callback	kernelCallback;
+	//tapkee::float_features_callback	featureCallback;
+
+	// Get the pixel neighborhoods and a buffer to store the projected pixels to.
+	cv::Mat projected, neighborhoods = this->getPixelNeighborhoods(exemplar, uv);
+
+	// Convert into an Eigen matrix.
+	Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic> eigenNeighbors;
+	cv::cv2eigen(neighborhoods.t(), eigenNeighbors);
+
+	// Apply PCA.
+	tapkee::ParametersSet parameters = tapkee::kwargs[
+		tapkee::method = tapkee::PCA,
+		tapkee::target_dimension = 4
+	];
+
+	tapkee::TapkeeOutput result = tapkee::initialize()
+		.withParameters(parameters)
+		//.withDistance(distanceCallback)
+		//.withFeatures(featureCallback)
+		//.withKernel(kernelCallback)
+		.embedUsing(eigenNeighbors);
+
+	// Convert it back.
+	cv::eigen2cv(result.embedding, projected);
+
+	// Return the projected neighborhoods.
+	TEXTURIZE_ASSERT(projected.rows == exemplar.channels());
+
+	return projected.t();
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///// t-SNE descriptor extractor implementation                                               /////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+cv::Mat Tapkee::SNEDescriptorExtractor::calculateNeighborhoodDescriptors(const Sample& exemplar) const
+{
+
+
+	throw;
+}
+
+cv::Mat Tapkee::SNEDescriptorExtractor::calculateNeighborhoodDescriptors(const Sample& exemplar, const cv::Mat& uv) const
+{
+	throw;
+}
