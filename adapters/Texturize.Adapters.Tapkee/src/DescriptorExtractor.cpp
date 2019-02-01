0da4e45a0dd849b2fc61e34c951ce2436fe3f65e
Carsten <18394207+Aschratt@users.noreply.github.com>
2019-02-01 12:42:44 +0100

    Implemented t-SNE based descriptor extractor for testing.

--- adapters/Texturize.Adapters.Tapkee/src/DescriptorExtractor.cpp
+++ adapters/Texturize.Adapters.Tapkee/src/DescriptorExtractor.cpp
@@ -28,7 +28,7 @@ cv::Mat Tapkee::PCADescriptorExtractor::calculateNeighborhoodDescriptors(const S
 	cv::Mat projected, neighborhoods = this->getPixelNeighborhoods(exemplar, uv);
 	
 	// Convert into an Eigen matrix.
-	tapkee::DenseMatrix eigenNeighbors;
+	tapkee::DenseMatrix eigenNeighbors, embeddedResult;
 	cv::cv2eigen(neighborhoods, eigenNeighbors);
 
 	// Apply PCA.
@@ -56,10 +56,37 @@ cv::Mat Tapkee::PCADescriptorExtractor::calculateNeighborhoodDescriptors(const S
 
 cv::Mat Tapkee::SNEDescriptorExtractor::calculateNeighborhoodDescriptors(const Sample& exemplar) const
 {
-	throw;
+	// Create a UV-Map for the sample.
+	cv::Mat uv = this->createContinuousUvMap(exemplar);
+
+	// Calculate the descriptors.
+	return this->calculateNeighborhoodDescriptors(exemplar, uv);
 }
 
 cv::Mat Tapkee::SNEDescriptorExtractor::calculateNeighborhoodDescriptors(const Sample& exemplar, const cv::Mat& uv) const
 {
-	throw;
+	// Get the pixel neighborhoods and a buffer to store the projected pixels to.
+	cv::Mat projected, neighborhoods = this->getPixelNeighborhoods(exemplar, uv);
+
+	// Convert into an Eigen matrix.
+	tapkee::DenseMatrix eigenNeighbors;
+	cv::cv2eigen(neighborhoods, eigenNeighbors);
+
+	// Apply PCA.
+	tapkee::ParametersSet parameters = tapkee::kwargs[
+		tapkee::method = tapkee::tDistributedStochasticNeighborEmbedding,
+		tapkee::target_dimension = exemplar.channels()
+	];
+
+	tapkee::TapkeeOutput result = tapkee::initialize()
+		.withParameters(parameters)
+		.embedUsing(eigenNeighbors);
+
+	// Convert it back.
+	cv::eigen2cv(result.embedding, projected);
+
+	// Return the projected neighborhoods.
+	TEXTURIZE_ASSERT(projected.cols == exemplar.channels());
+
+	return projected;
 }
