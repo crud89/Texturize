5e13c394cdc28720b60112182bbff7df06febcb2
Carsten <18394207+Aschratt@users.noreply.github.com>
2019-02-27 15:24:01 +0100

    Implemented MR8 filter bank app using anisotropic gaussian.

--- apps/Texturize.FilterMR8/Texturize.FilterMR8.cpp
+++ apps/Texturize.FilterMR8/Texturize.FilterMR8.cpp
@@ -10,6 +10,8 @@
 
 #include <opencv2/highgui.hpp>
 
+#include "anigauss.h"
+
 using namespace Texturize;
 
 // Command line parameter meta data.
@@ -73,24 +75,72 @@ int main(int argc, const char** argv) {
 	}
 
 	// Create a filter bank instance.
-	MaxResponseFilterBank filterBank(ksize);
+	//MaxResponseFilterBank filterBank(ksize);
 
 	// Filter the sample.
 	std::cout << "Executing... ";
 	Sample result;
 	auto start = std::chrono::high_resolution_clock::now();
-	filterBank.apply(result, sample);
+	//filterBank.apply(result, sample);
+
+	{
+		// Compute mean of each row.
+		cv::Mat image, output(sample.size(), CV_64FC1);
+		((cv::Mat)sample).convertTo(image, CV_64FC1);
+		cv::Scalar mean = cv::mean(image);
+		cv::subtract(image, mean, image);
+		cv::Mat sqrImg;
+		cv::multiply(image, image, sqrImg);
+		mean = cv::mean(sqrImg);
+		mean[0] = sqrt(mean[0]);
+		cv::divide(image, mean, image);
+
+		// The result is a 8-channel image of the same size as the input.
+		result = Sample(8, image.size());
+
+		double sfac{ 1.0 }, mulfac{ 2.0 }, s1{ 3.0 /* * sfac*/ }, s2{ 1.0 /* * sfac*/ };
+		int channel(0);
+
+		for (int mode(0); mode < 2; ++mode) {
+			cv::Mat r1 = cv::Mat::zeros(image.size(), CV_32FC1), r2 = cv::Mat::zeros(image.size(), CV_32FC1), buffer(image.size(), CV_32FC1);
+
+			for (int scale(0); scale < 5; ++scale) {
+				double phi = scale / 6.0 * 180.0;	// `anigauss` requires deg.
+				
+				anigauss(reinterpret_cast<double*>(image.data), reinterpret_cast<double*>(output.data), image.cols, image.rows, s1, s2, phi, 0, 1);
+				output.convertTo(buffer, CV_32FC1);
+				buffer = cv::abs(buffer);
+				cv::max(r1, buffer, r1);
+
+				anigauss(reinterpret_cast<double*>(image.data), reinterpret_cast<double*>(output.data), image.cols, image.rows, s1, s2, phi, 0, 2);
+				output.convertTo(buffer, CV_32FC1);
+				cv::max(r2, buffer, r2);
+			}
+
+			result.setChannel(channel++, r1);
+			result.setChannel(channel++, r2);
+
+			s1 *= mulfac;
+			s2 *= mulfac;
+		}
+
+		cv::Mat gaussian = cv::Mat::zeros(image.size(), CV_32FC1), laplacian = cv::Mat::zeros(image.size(), CV_32FC1);
+		double sigma = 10.0 * sfac;
+
+		anigauss(reinterpret_cast<double*>(image.data), reinterpret_cast<double*>(output.data), image.cols, image.rows, sigma, sigma, 0.0, 2, 0);
+		output.convertTo(gaussian, CV_32FC1);
+		anigauss(reinterpret_cast<double*>(image.data), reinterpret_cast<double*>(output.data), image.cols, image.rows, sigma, sigma, 0.0, 0, 2);
+		output.convertTo(laplacian, CV_32FC1);
+		cv::add(gaussian, laplacian, gaussian);
+		result.setChannel(channel++, gaussian);
+
+		anigauss(reinterpret_cast<double*>(image.data), reinterpret_cast<double*>(output.data), image.cols, image.rows, sigma, sigma, 0.0, 0, 0);
+		output.convertTo(gaussian, CV_32FC1);
+		result.setChannel(channel++, gaussian);
+	}
+
 	auto end = std::chrono::high_resolution_clock::now();
 	std::cout << " Done! (" << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms)" << std::endl;
-
-
-	for (int i(0); i < result.channels(); ++i)
-	{
-		cv::Mat filtered = result.getChannel(i);
-		std::cout << filtered;
-		cv::imshow("Filtered", filtered);
-		cv::waitKey(0);
-	}
 
 	// Store the sample.
 	_persistence.saveSample(resultFileName, result);
