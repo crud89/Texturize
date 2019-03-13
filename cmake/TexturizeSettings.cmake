1282a92efa5ddb12006619760ff3534df4ebb212
Carsten <18394207+Aschratt@users.noreply.github.com>
2019-03-13 11:45:05 +0100

    Added synthesizer app.

--- cmake/TexturizeSettings.cmake
+++ cmake/TexturizeSettings.cmake
@@ -13,6 +13,7 @@ OPTION(BUILD_APP_KMEANS             "Builds k-means clustering app."            
 OPTION(BUILD_APP_DISTANCE_MATRIX    "Builds app that computes pairwise distances."                                               ON)
 OPTION(BUILD_APP_PROGRESSION_MAP    "Builds scalar progression map generation app."                                              ON)
 OPTION(BUILD_APP_GUIDANCE_REFINE    "Builds app for target guidance refinement."                                                 ON)
+OPTION(BUILD_APP_SYNTHESIZE         "Builds synthesizer app."                                                                    ON)
 
 IF("${OpenCV_DIR}" STREQUAL "")
   SET(OpenCV_DIR "${CMAKE_MODULE_PATH}/opencv")
