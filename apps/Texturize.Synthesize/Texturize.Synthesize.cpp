e11f856241b658efcb17676afca8e1ed730f3b01
Carsten <18394207+Aschratt@users.noreply.github.com>
2019-03-14 14:50:46 +0100

    Synthesize app loads source and target progression maps, if provided.

--- apps/Texturize.Synthesize/Texturize.Synthesize.cpp
+++ apps/Texturize.Synthesize/Texturize.Synthesize.cpp
@@ -21,6 +21,10 @@ const char* parameters =
 	"{input in          |    | The name of an appearance space asset.}"
 	"{result r          |    | The name of the image file, the result uv map stored to.}"
 	"{seed s            | 0  | The seed to initialize random number generators with.}"
+	"{srcprog           |    | The name of a greyscale image, containing the source texture homogeneity progression.}"
+	"{trgprog           |    | The name of a greyscale image, containing the control homogeneity progression for the target texture.}"
+	"{homogeneity h     | 0.9| The importance factor of the source and target homgeneneity on the result.}"
+	/*"{anisotropy ai     | 0.5| The importance of local anisotropy, i.e. how much weight goes into homogeneity progression and how much into orientation differences.}"*/
 };
 
 // Persistence providers.
@@ -49,18 +53,56 @@ int main(int argc, const char** argv) {
 	// Print parameters.
 	std::string inputFileName = parser.get<std::string>("input");
 	std::string resultFileName = parser.get<std::string>("result");
+	std::string sourceProgressionFileName = parser.get<std::string>("srcprog");
+	std::string targetProgressionFileName = parser.get<std::string>("trgprog");
 	uint64_t seed = parser.get<uint64_t>("seed");
+	float homogeneity = parser.get<float>("homogeneity");
 
 	std::cout << "Input: " << inputFileName << std::endl <<
 		"Output: " << resultFileName << std::endl <<
-		std::endl;
+		"Seed: " << seed << std::endl;
+
+	// Non-stationary synthesis can only be done, if a source homogeneity map is provided.
+	if (sourceProgressionFileName.empty()) {
+		std::cout << "Source progression channel: " << sourceProgressionFileName << std::endl;
+
+		if (!targetProgressionFileName.empty())
+			std::cout << "Target progression channel: " << targetProgressionFileName << std::endl;
+		else
+			std::cout << "Target progression channel: not provided" << std::endl;
+	} else {
+		std::cout << "Source progression channel: ignored" << std::endl <<
+			"Target progression channel: ignored" << std::endl;
+
+		// Homogenious synthesis does not need weights for stationarity maps.
+		homogeneity = 0.f;
+	}
+
+	std::cout << "Homogeneity: " << homogeneity << std::endl << std::endl;
 
 	// Declare all samples and variables required.
-	Sample sample, exemplar, result;
+	Sample sample, exemplar, result, srcProgression, trgProgression;
 	int kernel, width, height;
 
-	// Load the input sample.
+	// Load the input samples, if they are provided.
 	_persistence.loadSample(inputFileName, sample);
+	srcProgression = Sample(cv::Mat::zeros(sample.size(), CV_32FC1));
+	trgProgression = Sample(cv::Mat::zeros(sample.size(), CV_32FC1));
+
+	if (!sourceProgressionFileName.empty()) {
+		_persistence.loadSample(sourceProgressionFileName, srcProgression);
+
+		if (!targetProgressionFileName.empty())
+			_persistence.loadSample(targetProgressionFileName, trgProgression);
+
+
+		// TODO: Weight the progression maps, based on the homogeneity.
+		// TODO: Pass target progression map during synthesis.
+		// TODO: Automatic jitter amplitude, based on variances.
+	}
+
+	TEXTURIZE_ASSERT(srcProgression.channels() == 1);
+	TEXTURIZE_ASSERT(trgProgression.channels() == 1);
 
 	// Load the appearance space descriptor.
 	std::unique_ptr<AppearanceSpace> descriptor;
@@ -78,8 +120,9 @@ int main(int argc, const char** argv) {
 	//TEXTURIZE_ASSERT(height == width);
 
 	// Build up the search index.
-	//std::shared_ptr<ISearchIndex> index = std::make_shared<CoherentIndex>(std::move(descriptor));
-	std::shared_ptr<ISearchIndex> index = std::make_shared<RandomWalkIndex>(std::move(descriptor));
+	//std::unique_ptr<IDescriptorExtractor> descriptorExtractor = std::make_unique<Tapkee::PCADescriptorExtractor>();
+	//std::shared_ptr<ISearchIndex> index = std::make_shared<KNNIndex>(std::move(descriptor), std::move(descriptorExtractor));
+	std::shared_ptr<ISearchIndex> index = std::make_shared<KNNIndex>(std::move(descriptor));
 
 #ifdef _DEBUG
 	auto synthesizer = PyramidSynthesizer::createSynthesizer(index);
